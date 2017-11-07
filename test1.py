fec4207892ce3ea0750899a7085a2ff3b4d9c141
Zhiwei Zhang <zzw408673530@gmail.com>
2017-11-07 00:32:53 -0600

    test

--- test1.py
+++ test1.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import numpy as np
 
+
+
+
+
 df = pd.read_csv('Demographic_Statistics_By_Zip_Code.csv')
 
 
-print(df)
+
+
+print(df[df['PERCENT PUBLIC ASSISTANCE TOTAL']>1]['PERCENT PUBLIC ASSISTANCE TOTAL'].head(20))
